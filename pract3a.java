9d9edef45b32b5eed09f10c7d1b1253c33d31097
Ameya10012003 <kulkarniameya.10@gmail.com>
2023-04-08 11:21:43 +0530

    added java program

---
+++ pract3a.java
@@ -0,0 +1,14 @@
+//JAVA PROGRAM FOR LEAP YEAR
+import java.io.*;
+class pract3a
+	{
+	 public static void main(String args[])
+		{
+		 int year;
+		 year=Integer.parseInt(args[0]);
+		 if(year%4==0)
+			{
+			 System.out.println("Year:"+year+"is Leap Year");
+			}
+		}
+	}
